55ba14badef717838621cce8f737f94bad4010f2
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-09-10 11:44:07 -0400

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Education from "./components/Education/Education";
+import Services from "./components/Services/Services";
+import Works from "./components/Works/Works";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <About />
+      <Skills />
+      <Education />
+      <Services />
+      <Works />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
