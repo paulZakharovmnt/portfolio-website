a058adcb25bd0ec482f49cbf37a630177a1dcc42
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-09-12 11:56:48 -0400

    add new info to pages, testing new style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,77 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about section" id="about">
+      <span className="section-subtitle">My intro</span>
+      <h2 className="section-title">About Me</h2>
+
+      <div className="about_container bd-grid">
+        <div className="about_data">
+          <p className="about_description">I'm ....blablabla</p>
+          One More my Photo
+          {/* <img src={aboutImg} alt="" className="about_img" /> */}
+        </div>
+
+        <div>
+          <div className="about_information">
+            <h3 className="about_information-title">Information</h3>
+
+            <div className="about_information-data">
+              <i className="fas fa-user about_information-icon"></i>
+              <span>Paul Zakharov</span>
+            </div>
+
+            <div className="about_information-data">
+              <i className="fas fa-phone-square-alt about_information-icon"></i>
+              <span>438-530-1015</span>
+            </div>
+
+            <div className="about_information-data">
+              <i className="fas fa-envelope about_information-icon "></i>
+              <span>paulZakharov.mnt@gmail.com</span>
+            </div>
+          </div>
+
+          <div className="about_information">
+            <h3 className="about_information-title">Experience and support</h3>
+
+            <div className="about_information-data">
+              <i className="fas fa-info-circle about_information-icon"></i>
+              <div>
+                <span className="about_information-subtitle">2 Years Job </span>
+                <span className="about_information-subtitle-small">
+                  Experience
+                </span>
+              </div>
+            </div>
+
+            <div className="about_information-data">
+              <i className="fas fa-briefcase about_information-icon"></i>
+              <div>
+                <span className="about_information-subtitle">
+                  Different Projects
+                </span>
+                <span className="about_information-subtitle-small">
+                  Completed
+                </span>
+              </div>
+            </div>
+
+            <div className="about_information-data">
+              <i className="fas fa-question-circle about_information-icon"></i>
+              <div>
+                <span className="about_information-subtitle">Support</span>
+                <span className="about_information-subtitle-small">
+                  Online 24/7
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,7 +2,43 @@ import React from "react";
 import "./Skills.css";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <section className="skills section" id="skills">
+      <span className="section-subtitle">Why Choose Me</span>
+      <h2 className="section-title">My Experience Area</h2>
+
+      <div className="skills_contatiner bd-grid">
+        <div className="skills_content">
+          <h3 className="skills_subtitle"> Frontend</h3>
+          <div className="skills_data">
+            <span className="skills_name">React.js </span>
+            <span className="skills_number">80% </span>
+            <span className="skills_bar skills_react"></span>
+          </div>
+          <div className="skills_data">
+            <span className="skills_name">React Hooks </span>
+            <span className="skills_number">80% </span>
+            <span className="skills_bar skills_react"></span>
+          </div>
+          <div className="skills_data">
+            <span className="skills_name">JavaScript </span>
+            <span className="skills_number">70% </span>
+            <span className="skills_bar skills_js"></span>
+          </div>
+          <div className="skills_data">
+            <span className="skills_name">Html/css </span>
+            <span className="skills_number">70% </span>
+            <span className="skills_bar skills_html"></span>
+          </div>
+          <div className="skills_data">
+            <span className="skills_name">TypeScript </span>
+            <span className="skills_number">50% </span>
+            <span className="skills_bar skills_typeScript"></span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
