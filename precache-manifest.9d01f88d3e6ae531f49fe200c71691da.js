294f41124d5a0056ae378667de518c3959975ff9
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-09-15 08:47:25 -0400

    Updates

---
+++ precache-manifest.9d01f88d3e6ae531f49fe200c71691da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1243da84f1d570fe665b7ce0ac9858b",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "eed28e1c6641c275ab60",
+    "url": "/portfolio-website/static/css/main.f0a08c03.chunk.css"
+  },
+  {
+    "revision": "d51c849c4041c223eaf6",
+    "url": "/portfolio-website/static/js/2.9fbe5dd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.9fbe5dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed28e1c6641c275ab60",
+    "url": "/portfolio-website/static/js/main.c1179668.chunk.js"
+  },
+  {
+    "revision": "25b38d133c1b66afda38",
+    "url": "/portfolio-website/static/js/runtime-main.8cd05721.js"
+  },
+  {
+    "revision": "e3d8755d9da8bb38d12d5f926cf33533",
+    "url": "/portfolio-website/static/media/Portfolio.e3d8755d.jpg"
+  },
+  {
+    "revision": "ea604922d5a805146820a657151a4b7b",
+    "url": "/portfolio-website/static/media/WeatherApp.ea604922.jpg"
+  },
+  {
+    "revision": "5c3a9601fe8357b981bff5c1ef8951b4",
+    "url": "/portfolio-website/static/media/me2.5c3a9601.jpg"
+  },
+  {
+    "revision": "900bf4c36abd33732022089a4410f1ed",
+    "url": "/portfolio-website/static/media/photo2.900bf4c3.png"
+  },
+  {
+    "revision": "09b0176f1df222939a6e3a48454a43db",
+    "url": "/portfolio-website/static/media/work4.09b0176f.jpg"
+  }
+]);
