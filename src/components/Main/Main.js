66161af5cdc7cb479f0110dd4d04ba3e44a1508f
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-17 10:14:53 -0500

    Change resume download link

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -38,7 +38,7 @@ const Main = ({ handleShowMessaheClick }) => {
             </div>
             <a
               download=""
-              href="https://drive.google.com/u/0/uc?id=1uMfim0ZE2penuMLjPPp1jJcGQu1-qNw2&export=download"
+              href="https://drive.google.com/u/0/uc?id=1LcAlX6FwhXqzMFFA-rb48x8j9lZd457R&export=download"
               className="btn home_button"
             >
               Download Resume
